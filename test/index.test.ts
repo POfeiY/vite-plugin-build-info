819232a160e025d233f7578d28bbe81adf3c4112
yuzili <395345203@qq.com>
2024-04-17 23:02:43 +0800

    test: update testing case.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,14 @@
 import { describe, expect, it } from 'vitest'
 import { getAppInfo, getGitInfo } from '../src/utils'
+import BuildInfoToHtml from '../src'
 
 describe('should', () => {
   it('getAppInfo', () => {
     expect(getAppInfo()).toMatchInlineSnapshot(`
       {
-        "buildStamp": "2024/4/13 22:05:54",
+        "buildStamp": "2024/4/17 23:02:15",
         "name": "vite-plugin-build-info-to-html",
-        "version": "0.0.1",
+        "version": "0.1.1-beta.4",
       }
     `)
   })
@@ -16,9 +17,17 @@ describe('should', () => {
     expect(gitInfo).toMatchInlineSnapshot(`
       {
         "branch": "main",
-        "commitHash": "c7e203b",
-        "describe": "c7e203b",
+        "commitHash": "19ae677",
+        "describe": "v0.1.1-beta.4-5-g19ae677",
         "username": "yuzili",
+      }
+    `)
+  })
+  it('buildInfoToHtml', () => {
+    expect(BuildInfoToHtml()).toMatchInlineSnapshot(`
+      {
+        "name": "vite-plugin-build-info-to-html",
+        "transformIndexHtml": [Function],
       }
     `)
   })
